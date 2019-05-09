4ca4b5dce4a984f082a4518fd2faa61108413505
Coxhuang <mhesat@outlook.com>
2019-05-09 16:25:40 +0800

    update

--- 2.Leetcode/21. 合并两个有序链表/1.merge-two-sorted-lists.py
+++ 2.Leetcode/21. 合并两个有序链表/1.merge-two-sorted-lists.py
@@ -1,16 +1,36 @@
+"""
+https://leetcode-cn.com/problems/merge-two-sorted-lists/submissions/
+"""
+
 # Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
 
 
 class Solution:
+    def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
 
-    def mergeTwoLists(self, l1, l2):
+        list_node = []
 
+        if l1:
+            while l1.next:
 
+                if l1.next:
+                    list_node.append(l1.val)
+                    l1 = l1.next
+            else:
+                list_node.append(l1.val)
+        if l2:
+            while l2.next:
 
-        print("aa")
+                if l2.next:
+                    list_node.append(l2.val)
+                    l2 = l2.next
+            else:
+                list_node.append(l2.val)
 
-        return None
+        list_node = sorted(list_node)
+
+        return list_node
