5096345562d3a00636fa045f386909cb573fa326
Coxhuang <mhesat@outlook.com>
2019-05-16 17:20:04 +0800

    update

--- text.py
+++ text.py
@@ -1,10 +1,11 @@
-import requests
+l1 = [1, 2, 3, 4]
+l2 = [2, 3, 4, 5]
+l3 = list(zip(l1, l2))
 
+print(l3)
+for i in l3:
+    print('for循环{}'.format(i))
 
+l4 = [x for x in l3]
+print(l4)
 
-
-r  =requests.get(
-    url="http://127.0.0.1:8000/view_1/",
-)
-
-print(r.text)
