e2b9fb4646956a1de6d88b85567038fec20d845c
Coxhuang <mhesat@outlook.com>
2019-05-20 19:41:08 +0800

    update

--- 1.Algorithm/3.经典算法/1.Fibonacci/3.递归.py
+++ 1.Algorithm/3.经典算法/1.Fibonacci/3.递归.py
@@ -7,5 +7,7 @@ def fib(n):
 
     return fib(n-1) + fib(n-2)
 
-print(fib(10))
+for foo in  range(10):
+
+    print(fib(foo))
 
--- 1.Algorithm/3.经典算法/2.台阶问题/1.func.py
+++ 1.Algorithm/3.经典算法/2.台阶问题/1.func.py
@@ -2,3 +2,20 @@
 题目描述：一只青蛙一次可以跳上1级台阶，也可以跳上2级。求该青蛙跳上一个n级的台阶总共有多少种方法？
 """
 
+
+
+def fib(max_val):
+
+    a, b, n = 0, 1, max_val
+
+    while n:
+
+        a, b = b, a+b
+        n -= 1
+
+    return b
+
+n = 3
+
+ret = n if n <= 2 else fib(n)
+print(ret)
