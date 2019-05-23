486da224dda446812ccfb40116e702af7196fad9
Coxhuang <mhesat@outlook.com>
2019-05-23 15:12:02 +0800

    update

--- 2.Leetcode/26. 删除排序数组中的重复项/1.remove-duplicates-from-sorted-array.py
+++ 2.Leetcode/26. 删除排序数组中的重复项/1.remove-duplicates-from-sorted-array.py
@@ -1,1 +1,13 @@
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
 
+        for i, num in enumerate(nums):
+
+            try:
+
+                while num == nums[i + 1]:
+                    nums.pop(i + 1)
+            except:
+                break
+
+        return len(nums)
